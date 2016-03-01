a3cece23d8797e47107f4df83556236e33cefc8b
peteTheDiscoPig <peteroconnor1989@gmail.com>
2016-03-01 19:24:55 +0000

    test jenkins

--- src/main/java/com/peteroconnor/fyp/SpeakerAuthentication/speechRecognition/TestWatson.java
+++ src/main/java/com/peteroconnor/fyp/SpeakerAuthentication/speechRecognition/TestWatson.java
@@ -1,21 +1,21 @@
-//package com.peteroconnor.fyp.SpeakerAuthentication.speechRecognition;
-//
-//import java.io.File;
-//
-//import com.peteroconnor.fyp.SpeakerAuthentication.Entity.AudioData;
-//
-//public class TestWatson {
-//
-//	public static void main(String[] args) {
-//		File file = new File(AudioData.VOICE_FILE_LOCATION);
-//		String prompt = "the quick brown fox jumped over the lazy dog";
-//		
-//		SpeechRecognition sr = new SpeechRecognition();
-//		sr.transcriptUsingHTTP(file);
-//		
-//		System.out.println(sr.getSpokenPhrase());
-//		
-//		System.out.println(sr.isPhraseMatch(prompt));
-//
-//	}
-//}
+package com.peteroconnor.fyp.SpeakerAuthentication.speechRecognition;
+
+import java.io.File;
+
+import com.peteroconnor.fyp.SpeakerAuthentication.Entity.AudioData;
+
+public class TestWatson {
+
+	public static void main(String[] args) {
+		File file = new File(AudioData.VOICE_FILE_LOCATION);
+		String prompt = "the quick brown fox jumped over the lazy dog";
+		
+		SpeechRecognition sr = new SpeechRecognition();
+		sr.transcriptUsingHTTP(file);
+		
+		System.out.println(sr.getSpokenPhrase());
+		
+		System.out.println(sr.isPhraseMatch(prompt));
+
+	}
+}
