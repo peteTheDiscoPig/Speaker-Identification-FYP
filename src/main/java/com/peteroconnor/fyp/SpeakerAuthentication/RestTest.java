4358a3329ec92b68027f19ea443cd44c3f4248de
peteTheDiscoPig <peteroconnor1989@gmail.com>
2016-04-25 20:31:03 +0100

    rest test

---
+++ src/main/java/com/peteroconnor/fyp/SpeakerAuthentication/RestTest.java
@@ -0,0 +1,15 @@
+package com.peteroconnor.fyp.SpeakerAuthentication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestTest {
+	
+	@RequestMapping("/ASV/test")
+	public String sendMessage(){
+		
+		return "Hello World XXX";
+	}
+}
