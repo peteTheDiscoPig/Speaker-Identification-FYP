a33ed470b5c2291c5563c06a97fdc0be4a24645b
peteTheDiscoPig <peteroconnor1989@gmail.com>
2016-04-25 22:54:46 +0100

    test

--- src/main/java/com/peteroconnor/fyp/SpeakerAuthentication/Application.java
+++ src/main/java/com/peteroconnor/fyp/SpeakerAuthentication/Application.java
@@ -1,14 +1,25 @@
 package com.peteroconnor.fyp.SpeakerAuthentication;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
-@Profile("cloud")
 public class Application {
 	public static void main(String[] args){
-		SpringApplication.run(Application.class, args);
+		//SpringApplication.run(Application.class, args);
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
 	}
 
 }
