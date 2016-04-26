df8fc73b0b21edb572d942dadede4876ee10c826
peteTheDiscoPig <peteroconnor1989@gmail.com>
2016-04-26 10:11:42 +0100

    web.xml

--- src/main/java/com/peteroconnor/fyp/SpeakerAuthentication/Application.java
+++ src/main/java/com/peteroconnor/fyp/SpeakerAuthentication/Application.java
@@ -4,11 +4,20 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
 
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer implements WebApplicationInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+	
 	public static void main(String[] args){
 		//SpringApplication.run(Application.class, args);
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
